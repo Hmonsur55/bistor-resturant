0c5c289ab208467bae125633ccfe6bfac6ca8971
Hmonsur55 <116294687+Hmonsur55@users.noreply.github.com>
2023-05-27 09:50:01 +0600

    add cateogry text

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -14,11 +14,10 @@ import slide7 from '../../../assets/home/slide3.jpg'
 import slide8 from '../../../assets/home/slide5.jpg'
 const Category = () => {
     return (
-      <div className="max-w-screen-xl mx-auto">
+      <div className="max-w-screen-xl mx-auto mb-6">
         <Swiper
           slidesPerView={4}
           spaceBetween={30}
-
           pagination={{
             clickable: true,
           }}
@@ -27,27 +26,51 @@ const Category = () => {
         >
           <SwiperSlide>
             <img src={slide1} alt="" />
+            <p className="-mt-16 text-center uppercase text-3xl text-white">
+              Salads
+            </p>
           </SwiperSlide>
           <SwiperSlide>
             <img src={slide2} alt="" />
+            <p className="-mt-16 text-center uppercase text-3xl text-white">
+              Pizza
+            </p>
           </SwiperSlide>
           <SwiperSlide>
             <img src={slide3} alt="" />
+            <p className="-mt-16 text-center uppercase text-3xl text-white">
+              Shup
+            </p>
           </SwiperSlide>
           <SwiperSlide>
             <img src={slide4} alt="" />
+            <p className="-mt-16 text-center uppercase text-3xl text-white">
+              Desarts
+            </p>
           </SwiperSlide>
           <SwiperSlide>
             <img src={slide5} alt="" />
+            <p className="-mt-16 text-center uppercase text-3xl text-white">
+              Salads
+            </p>
           </SwiperSlide>
           <SwiperSlide>
-            <img src={slide6} alt="" />
+            <img src={slide2} alt="" />
+            <p className="-mt-16 text-center uppercase text-3xl text-white">
+              Pizza
+            </p>
           </SwiperSlide>
           <SwiperSlide>
             <img src={slide7} alt="" />
+            <p className="-mt-16 text-center uppercase text-3xl text-white">
+              Soup
+            </p>
           </SwiperSlide>
           <SwiperSlide>
             <img src={slide8} alt="" />
+            <p className="-mt-16 text-center uppercase text-3xl text-white">
+              Salads
+            </p>
           </SwiperSlide>
         </Swiper>
       </div>
