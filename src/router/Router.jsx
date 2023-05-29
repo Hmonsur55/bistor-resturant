97a845f548e3f9b33241cd4fd214e7a243b319a2
Hmonsur55 <116294687+Hmonsur55@users.noreply.github.com>
2023-05-29 10:49:55 +0600

    add login page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
+import Login from "../pages/login/Login";
 import Menu from "../pages/Menu/Menu";
 import Order from "../pages/Order/Order";
+import Registration from "../Registration/Registration";
 
 export const router = createBrowserRouter([
   {
@@ -16,9 +18,18 @@ export const router = createBrowserRouter([
       {
         path: '/menu',
         element: <Menu></Menu>
-      }, {
+      },
+      {
         path: '/order/:category',
         element: <Order></Order>
+      },
+      {
+        path: '/login',
+        element:<Login></Login>
+      },
+      {
+        path: '/registration',
+        element:<Registration></Registration>
       }
     ],
   },
